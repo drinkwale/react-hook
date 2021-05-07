751575297e52b597bfb99ddffc2726bb2fed602f
drinkwale <jackslash88@gmail.com>
2021-05-07 20:14:12 +0900

    #1.0 Introduction to useState

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+const App = () => {
+    const [item, setItem] = useState(1);
+    const incrementItem = () => setItem(item + 1);
+    const decrementItem = () => setItem(item - 1);
+    return (
+        <div className="App">
+            <h1>Hello {item}</h1>
+            <button onClick={incrementItem}>Increment</button>
+            <button onClick={decrementItem}>Decrement</button>
+        </div>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
