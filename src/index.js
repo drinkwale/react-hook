16f9cd9f7e0e85c336ef44ac48a9aa12a8b2068e
drinkwale <jackslash88@gmail.com>
2021-05-09 19:09:39 +0900

    #2.1 useTitle

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    const sayHello = () => console.log("hello");
-    useEffect(() => {
-        sayHello();
-    });
+const useTitle = (initialTitle) => {
+    const [title, setTitle] = useState(initialTitle);
+    const updateTitle = () => {
+        const htmlTitle = document.querySelector("title");
+        htmlTitle.innerText = title;
+    };
+    useEffect(updateTitle, [title]);
+    return setTitle;
+};
 
-    const [number, setNumber] = useState(0);
-    const [aNumber, setAnumber] = useState(0);
-    return (
-        <div className="App">
-            <div>Hi</div>
-            <button onClick={() => setNumber(number + 1)}>{number}</button>
-            <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
-        </div>
-    );
+const App = () => {
+    const titleUpdater = useTitle("Loading ... ");
+    setTimeout(() => titleUpdater("Home"), 5000);
+    return <div className="App">Hi</div>;
 };
 
 const rootElement = document.getElementById("root");
